2e68b3229184b987deb459634f199e0a97c4c442
Olympian <99050671+OIympian@users.noreply.github.com>
2022-10-19 14:03:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+$("#form").submit(function (e) {
+    e.preventDefault()
+
+    var query = $("#search").val()
+    apikey = "0a274bd198bde10f3218287147180390"
+    var url = 'http://api.serpstack.com/search?access_key=' + apikey + '&type=web&query=' + query
+
+    let results = ''
+
+    $.get(url, function (data) {
+        console.log(data)
+
+        data.organic_results.forEach(res => {
+            results = `
+            <h1>${res.title}</h1><br><a href="${res.url}">${res.url}</a>
+            <p>${res.snippet}</p>
+
+            `
+            $("#results").append(results)
+        });
+    })
+})
+
